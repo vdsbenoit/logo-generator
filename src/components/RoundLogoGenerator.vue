e67735ecd4a2c525a5718f774ebc9b226f21b33a
vdsbenoit <vdsbenoit@gmail.com>
2024-09-23 20:37:32 +0200

    All frite.dev logos

--- src/components/RoundLogoGenerator.vue
+++ src/components/RoundLogoGenerator.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="h-svh w-screen bg-white">
-    <div id="capture" class="flex h-[132px] w-[320px] items-center justify-center bg-neutral-900">
+    <div
+      id="logo-full-dark"
+      class="flex h-[132px] w-[320px] items-center justify-center bg-neutral-900"
+    >
       <div class="h-[60%] rounded-full">
         <img class="size-full" src="@/assets/logo.png" />
       </div>
       <p class="ml-4 text-[2.7rem] font-bold text-neutral-100">frite.dev</p>
     </div>
 
-    <button class="ml-4 mt-10 rounded bg-gray-300 px-2 py-1 font-semibold" @click="captureAndSave">
+    <button
+      class="my-5 ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
+      @click="captureAndSave('logo-full-dark')"
+    >
+      Export as PNG
+    </button>
+
+    <div class="h-[132px] w-[320px] ring-1 ring-black">
+      <div id="logo-full-light" class="flex h-full w-full items-center justify-center">
+        <div class="h-[60%] rounded-full bg-neutral-900 p-3">
+          <img class="size-full" src="@/assets/logo.png" />
+        </div>
+        <p class="ml-4 text-[2.7rem] font-bold text-neutral-900">frite.dev</p>
+      </div>
+    </div>
+
+    <button
+      class="my-5 ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
+      @click="captureAndSave('logo-full-light')"
+    >
+      Export as PNG
+    </button>
+
+    <div
+      id="logo-round"
+      class="flex h-fit w-fit items-center justify-center rounded-full bg-neutral-900"
+    >
+      <img class="p-16" src="@/assets/logo.png" />
+    </div>
+
+    <button
+      class="my-5 ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
+      @click="captureAndSave('logo-round')"
+    >
       Export as PNG
     </button>
   </div>
@@ -18,9 +54,9 @@ import { nextTick } from "vue"
 import { toPng } from "html-to-image"
 import { saveAs } from "file-saver"
 
-const captureAndSave = async () => {
+const captureAndSave = async (elementName: string) => {
   await nextTick()
-  const element = document.getElementById("capture")
+  const element = document.getElementById(elementName)
 
   try {
     if (!element) {
@@ -28,7 +64,7 @@ const captureAndSave = async () => {
     }
     const dataUrl = await toPng(element)
     const blob = await (await fetch(dataUrl)).blob()
-    saveAs(blob, "new-logo.png")
+    saveAs(blob, `${elementName}.png`)
   } catch (error) {
     console.error("Error capturing and saving image:", error)
   }
