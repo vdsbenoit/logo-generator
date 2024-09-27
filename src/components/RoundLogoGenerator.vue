cbd21fad022cfcc98d8f13cfc6785bbd33030bb1
vdsbenoit <vdsbenoit@gmail.com>
2024-09-27 12:43:16 +0200

    Add transparent parameter

--- src/components/RoundLogoGenerator.vue
+++ src/components/RoundLogoGenerator.vue
@@ -34,34 +34,10 @@
 
     <div class="mt-5 grid grid-cols-2">
       <div class="h-[132px] w-[320px] ring-1 ring-black">
-        <div id="logo-full-light" class="flex h-full w-full items-center justify-center bg-white">
-          <div
-            class="h-[60%] rounded-full p-3"
-            style="background: radial-gradient(ellipse at bottom, #171717 0%, #0a0a0a 100%)"
-          >
-            <img class="size-full" src="@/assets/logo.png" />
-          </div>
-          <p class="ml-4 text-[2.7rem] font-bold text-neutral-950">frite.dev</p>
-        </div>
-      </div>
-
-      <div class="flex items-center">
-        <button
-          class="ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
-          @click="captureAndSave('logo-full-light')"
-        >
-          Export logo-full-light
-        </button>
-      </div>
-    </div>
-
-    <!-- Full transparent -->
-
-    <div class="mt-5 grid grid-cols-2">
-      <div class="h-[132px] w-[320px] ring-1 ring-black">
         <div
-          id="logo-full-transparent"
-          class="flex h-full w-full items-center justify-center bg-white"
+          id="logo-full-light"
+          class="flex h-full w-full items-center justify-center"
+          :class="{ 'bg-white': !isTransparent }"
         >
           <div
             class="h-[60%] rounded-full p-3"
@@ -74,12 +50,22 @@
       </div>
 
       <div class="flex items-center">
-        <button
-          class="ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
-          @click="captureAndSave('logo-full-transparent')"
-        >
-          Export logo-full-transparent
-        </button>
+        <div>
+          <button
+            class="ml-4 rounded bg-gray-300 px-2 py-1 font-semibold"
+            @click="captureAndSave('logo-full-light', isTransparent ? 'logo-full-transparent' : '')"
+          >
+            Export logo-full-{{ isTransparent ? "transparent" : "light" }}
+          </button>
+          <br />
+          <label for="is-transparent" class="mx-4">Transparent</label>
+          <input
+            id="is-transparent"
+            type="checkbox"
+            class="form-checkbox h-5 w-5 text-gray-600"
+            v-model="isTransparent"
+          />
+        </div>
       </div>
     </div>
 
@@ -107,11 +93,13 @@
 </template>
 
 <script lang="ts" setup>
-import { nextTick } from "vue"
+import { nextTick, ref } from "vue"
 import { toPng } from "html-to-image"
 import { saveAs } from "file-saver"
 
-const captureAndSave = async (elementName: string) => {
+const isTransparent = ref(false)
+
+const captureAndSave = async (elementName: string, exportName = "") => {
   await nextTick()
   const element = document.getElementById(elementName)
 
@@ -121,15 +109,17 @@ const captureAndSave = async (elementName: string) => {
     }
     const dataUrl = await toPng(element)
     const blob = await (await fetch(dataUrl)).blob()
-    saveAs(blob, `${elementName}.png`)
+    saveAs(blob, `${exportName === "" ? elementName : exportName}.png`)
   } catch (error) {
     console.error("Error capturing and saving image:", error)
   }
 }
 const exportAll = async () => {
   await captureAndSave("logo-full-dark")
+  isTransparent.value = false
   await captureAndSave("logo-full-light")
-  await captureAndSave("logo-full-transparent")
+  isTransparent.value = true
+  captureAndSave("logo-full-light", "logo-full-transparent")
   await captureAndSave("logo-round")
 }
 </script>
